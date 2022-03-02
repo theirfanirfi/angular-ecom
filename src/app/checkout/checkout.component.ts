80f2f255cf1cbc44945f5f29d1c1603250ed3de9
theirfanirfi <theirfanullah@gmail.com>
2022-03-02 19:00:28 +0500

    to deliver

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Cart } from '../models/cart.model'
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import { CartService } from '../services/cart.service'
 import { TotalpriceService } from '../services/totalprice.service'
 @Component({
@@ -10,6 +11,10 @@ import { TotalpriceService } from '../services/totalprice.service'
 export class CheckoutComponent implements OnInit {
   total_cart_price: number = 0;
   constructor(private cartservice: CartService, private totalprice: TotalpriceService) { }
+  form = new FormGroup({
+    "email": new FormControl("", Validators.required),
+    "password": new FormControl("", Validators.required),
+  });
 
   ngOnInit(): void {
     this.total_cart_price = this.totalprice.getTotalPrice();
