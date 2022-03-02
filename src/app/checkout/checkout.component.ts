fddcd7cbbc945a524bdc20fcd692a4c4d2f2b5bc
theirfanirfi <theirfanullah@gmail.com>
2022-03-02 18:46:09 +0500

    checkout form fixed

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Cart } from '../models/cart.model'
+import { CartService } from '../services/cart.service'
+import { TotalpriceService } from '../services/totalprice.service'
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
   styleUrls: ['./checkout.component.css']
 })
 export class CheckoutComponent implements OnInit {
-
-  constructor() { }
+  total_cart_price: number = 0;
+  constructor(private cartservice: CartService, private totalprice: TotalpriceService) { }
 
   ngOnInit(): void {
+    this.total_cart_price = this.totalprice.getTotalPrice();
   }
 
 }
